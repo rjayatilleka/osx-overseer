fec40adf00913304640934749c80f060aa235d2d
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-10-02 10:55:16 -0700

    Stuf

--- src/daemon.rs
+++ src/daemon.rs
@@ -5,6 +5,9 @@
 use types;
 use types::Homes;
 
+use std::io;
+use std::io::Write;
+use std::error::Error;
 use unix_socket::UnixDatagram;
 
 /// Represents errors in client state machine
@@ -24,35 +27,26 @@ pub enum DaemonState {
     ReceiveRequest(UnixDatagram),
     ProcessRequest(UnixDatagram, Request),
     SendResponse(UnixDatagram),
-    Exit(Result<(), DaemonError>),
+    Fatal(DaemonError),
     Done,
 }
 
 
 /// Start -> InitHomes
 fn handle_start() -> DaemonState {
-    println!("Start");
     DaemonState::InitHomes
 }
 
 
 /// InitHomes -> OpenSocket or Exit
 fn handle_init_homes() -> DaemonState {
-    println!("InitHomes");
-
     let homes = types::get_homes();
     println!("{:?}", homes);
-    DaemonState::Exit(Ok(()))
+    DaemonState::Done
 }
 
-fn handle_exit(result: Result<(), DaemonError>) -> DaemonState {
-    println!("Exit");
-
-    match result {
-        Ok(s) => println!("overseerd success"),
-        Err(e) => println!("{:?}", e),
-    }
-
+fn handle_fatal(e: DaemonError) -> DaemonState {
+    let _ = writeln!(&mut io::stderr(), "FATAL: {}", e.description());
     DaemonState::Done
 }
 
@@ -60,7 +54,7 @@ pub fn execute_daemon_step(state: DaemonState) -> Option<DaemonState> {
     match state {
         DaemonState::Start => Some(handle_start()),
         DaemonState::InitHomes => Some(handle_init_homes()),
-        DaemonState::Exit(r) => Some(handle_exit(r)),
+        DaemonState::Fatal(e) => Some(handle_fatal(e)),
         DaemonState::Done => None,
         _ => Some(DaemonState::Done),
     }
